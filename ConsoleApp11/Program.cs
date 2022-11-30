fe905de28f9b3b93c2e3b624e39c5bb29d7fae2c
Артём Коваленко <ak7527203@gmail.com>
2022-11-30 23:18:24 +0200

    all done

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,14 @@
+﻿using ClassLibrary1;
+
+double[,] mas = new double[4, 6];
+Random r = new Random();
+for (int i = 0; i < 4; i++)
+{
+    for (int j = 0; j < 6; j++)
+    {
+        mas[i, j] = r.NextDouble();
+        Console.Write($"{mas[i, j]:F2} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine(Class1.maxPair(mas)); 
